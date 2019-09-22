4fa47d22c91b39e7e28124b5cae9e8c79be0dab3
RTuxen <39836677+RTuxen@users.noreply.github.com>
2019-09-22 16:51:12 +0200

    Added higher figure resolution

--- ecoli.py
+++ ecoli.py
@@ -33,6 +33,7 @@ C = len(classNames)
 
 N, M = X1.shape
 # %% PCA
+
 # Make 1-of-K-encoding
 #onehot_encoder = OneHotEncoder(sparse=False,categories='auto')
 #y_reshaped = y.reshape(len(y), 1)
@@ -53,11 +54,11 @@ rho = (S*S) / (S*S).sum()
 # Project the centered data onto principal component space
 Z = Y @ V
 # %% Correlation
-plt.figure(dpi=300)
+f1 = plt.figure(dpi=300)
 corr = df2.corr()
 ax = sns.heatmap(corr,vmin=-1, vmax=1, center=0,square=True)
 plt.title('Variable Correlation')
-plt.savefig('./figures/correlation.png', bbox_inches='tight')
+f1.savefig('./figures/correlation.png', bbox_inches='tight')
 plt.show()
 
 
@@ -68,7 +69,7 @@ plt.show()
 i = 0
 j = 1
 
-f = plt.figure()
+f2 = plt.figure()
 plt.title('Ecoli data')
 
 for c in range(C):
@@ -83,7 +84,7 @@ plt.show()
 
 # Plot variance explained
 threshold = 0.9
-plt.figure()
+f3 = plt.figure()
 plt.plot(range(1,len(rho)+1),rho,'x-')
 plt.plot(range(1,len(rho)+1),np.cumsum(rho),'o-')
 plt.plot([1,len(rho)],[threshold, threshold],'k--')
@@ -98,7 +99,7 @@ plt.show()
 i = 0
 j = 1
 
-f = plt.figure()
+f4 = plt.figure(dpi=300)
 plt.title('Ecoli data: PCA')
 #Z = array(Z)
 for c in range(C):
@@ -108,6 +109,7 @@ for c in range(C):
 plt.legend(classNames)
 plt.xlabel('PC{0}'.format(i+1))
 plt.ylabel('PC{0}'.format(j+1))
+f4.savefig('./figures/PC1_PC2_plot.png', bbox_inches='tight')
 plt.show()
 
 # Bar plot of PCA
@@ -116,6 +118,7 @@ legendStrs = ['PC'+str(e+1) for e in pcs]
 c = ['r','g','b']
 bw = .2
 r = np.arange(1,M+1)
+f5 = plt.figure()
 for i in pcs:    
     plt.bar(r+i*bw, V[:,i], width=bw)
 plt.xticks(r+bw, attributeNames)
