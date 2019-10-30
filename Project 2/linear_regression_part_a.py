5a1e4fd62f2becfa09a82a4656cb02562f8ba4e7
RTuxen <39836677+RTuxen@users.noreply.github.com>
2019-10-30 16:45:27 +0100

    Code for regression part a is mostly done

---
+++ Project 2/linear_regression_part_a.py
@@ -0,0 +1,96 @@
+from matplotlib.pylab import (figure, semilogx, loglog, xlabel, ylabel, legend, 
+                           title, subplot, show, grid, plot)
+import numpy as np
+from scipy.io import loadmat
+import sklearn.linear_model as lm
+from sklearn import model_selection
+from toolbox_02450 import rlr_validate
+
+from read_ecoli_data import *
+
+
+# Select the last feature 'alm2'
+y = X[:,-1]
+X = X[:,0:-1]
+M = M-1
+attributeNames[-1] = []
+
+# Standardizes data matrix so each column has mean 0 and std 1
+X = (X - np.ones((N,1))*X.mean(0))/X.std(0)
+
+# Add offset attribute
+X = np.concatenate((np.ones((X.shape[0],1)),X),1)
+attributeNames = [u'Offset']+attributeNames
+M = M+1
+
+## Crossvalidation
+# Create crossvalidation partition for evaluation
+K = 10
+CV = model_selection.KFold(K, shuffle=True)
+
+
+# Values of lambda
+lambdas = np.power(10.,range(-3,5))
+lambdas = np.arange(0.1,100,0.5)
+
+# Initialize data
+w = np.empty((M,K,len(lambdas)))
+train_error = np.empty((K,len(lambdas)))
+test_error = np.empty((K,len(lambdas)))
+y = y.squeeze()
+
+
+k = 0
+for train_index, test_index in CV.split(X,y):
+    X_train = X[train_index]
+    y_train = y[train_index]
+    X_test = X[test_index]
+    y_test = y[test_index]
+    
+    
+    # precompute terms
+    Xty = X_train.T @ y_train
+    XtX = X_train.T @ X_train
+    
+    
+    for l in range(0,len(lambdas)):
+        # Compute parameters for current value of lambda and current CV fold
+        lambdaI = lambdas[l] * np.eye(M)
+        lambdaI[0,0] = 0 # remove bias regularization
+        w[:,k,l] = np.linalg.solve(XtX+lambdaI,Xty).squeeze()
+        # Evaluate training and test performance
+        train_error[k,l] = np.power(y_train-X_train @ w[:,k,l].T,2).mean(axis=0)
+        test_error[k,l] = np.power(y_test-X_test @ w[:,k,l].T,2).mean(axis=0)
+
+    k=k+1
+    
+minArg = np.argmin(np.mean(test_error,axis=0))
+
+opt_val_err = np.min(np.mean(test_error,axis=0))
+opt_lambda = lambdas[minArg]
+train_err_vs_lambda = np.mean(train_error,axis=0)
+test_err_vs_lambda = np.mean(test_error,axis=0)
+mean_w_vs_lambda = np.squeeze(np.mean(w,axis=1))
+
+
+f = figure()
+title('Optimal lambda: 1e{0}'.format(np.log10(opt_lambda)))
+semilogx(lambdas,train_err_vs_lambda.T,'b.-',lambdas,test_err_vs_lambda.T,'r.-')
+xlabel('Regularization factor')
+ylabel('Squared error (crossvalidation)')
+legend(['Train error','Validation error'])
+grid()
+
+# The difference from last plot here is that opt_lamda is not written as a power of 10
+f = figure()
+title('Optimal lambda: {0}'.format(opt_lambda))
+semilogx(lambdas,train_err_vs_lambda.T,'b.-',lambdas,test_err_vs_lambda.T,'r.-')
+xlabel('Regularization factor')
+ylabel('Squared error (crossvalidation)')
+legend(['Train error','Validation error'])
+grid()
+
+
+print('Weights for best regularization parameter:')
+for m in range(M):
+    print('{:>15} {:>15}'.format(attributeNames[m], np.round(mean_w_vs_lambda[m,minArg],3)))
