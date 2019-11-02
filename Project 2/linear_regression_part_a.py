6459faa8cb3a1315913b25349d38839894104a16
RTuxen <39836677+RTuxen@users.noreply.github.com>
2019-11-02 16:01:51 +0100

    Added one-of-K encoding

--- Project 2/linear_regression_part_a.py
+++ Project 2/linear_regression_part_a.py
@@ -5,23 +5,31 @@ from scipy.io import loadmat
 import sklearn.linear_model as lm
 from sklearn import model_selection
 from toolbox_02450 import rlr_validate
+from sklearn.preprocessing import OneHotEncoder
 
 from read_ecoli_data import *
 
+# one hot encoding of cell type
+ohe = OneHotEncoder(sparse=False,categories='auto')
+x = ohe.fit_transform(y.reshape(-1, 1))
 
-# Select the last feature 'alm2'
+# Select the feature 'alm2' as new predictor and remove from data set
 y = X[:,-1]
 X = X[:,0:-1]
-M = M-1
-attributeNames[-1] = []
+
+# Add one hot encoded to data set
+X = np.concatenate((X,x),1)
 
 # Standardizes data matrix so each column has mean 0 and std 1
 X = (X - np.ones((N,1))*X.mean(0))/X.std(0)
 
 # Add offset attribute
 X = np.concatenate((np.ones((X.shape[0],1)),X),1)
-attributeNames = [u'Offset']+attributeNames
-M = M+1
+
+# Set attribute names and shape
+attributeNames = attributeNames[0:-1]
+attributeNames = [u'Offset']+attributeNames + [u'cp'] + [u'im'] + [u'imL'] + [u'imS'] + [u'imU'] + [u'om'] + [u'omL'] + [u'pp']
+M = X.shape[1]
 
 ## Crossvalidation
 # Create crossvalidation partition for evaluation
@@ -30,8 +38,8 @@ CV = model_selection.KFold(K, shuffle=True)
 
 
 # Values of lambda
-lambdas = np.power(10.,range(-3,5))
-lambdas = np.arange(0.1,100,0.5)
+lambdas = np.power(10.,range(-1,7))
+#lambdas = np.arange(0.1,100,0.5)
 
 # Initialize data
 w = np.empty((M,K,len(lambdas)))
@@ -89,6 +97,15 @@ xlabel('Regularization factor')
 ylabel('Squared error (crossvalidation)')
 legend(['Train error','Validation error'])
 grid()
+
+f = figure()
+semilogx(lambdas,mean_w_vs_lambda.T[:,1:],'.-') # Don't plot the bias term
+xlabel('Regularization factor')
+ylabel('Mean Coefficient Values')
+grid()
+# You can choose to display the legend, but it's omitted for a cleaner 
+# plot, since there are many attributes
+#legend(attributeNames[1:], loc='best')
 
 
 print('Weights for best regularization parameter:')
