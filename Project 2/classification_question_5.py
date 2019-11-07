515ba1290628306146ef0b68c9e121080493eccc
RTuxen <39836677+RTuxen@users.noreply.github.com>
2019-11-07 16:55:52 +0100

    Changed train index and lambda

--- Project 2/classification_question_5.py
+++ Project 2/classification_question_5.py
@@ -10,16 +10,17 @@ from toolbox_02450 import mcnemar
 from read_ecoli_data import *
 
 # Mean op optimal lambda found from last exercise
-lambda_opt = 0.044
+lambda_opt = 0.06
 
-# Split into test and training set
-index = 303
 
-X_train = X[0:index,:]
-y_train = y[0:index]
-X_test = X[index-1:-1,:]
-y_test = y[index-1:-1]
-
+K = 10
+CV = model_selection.KFold(n_splits=K,shuffle=True, random_state = 1)
+for train_index, test_index in CV.split(X):
+    # extract training and test set for current CV fold
+    X_train = X[train_index,:]
+    y_train = y[train_index]
+    X_test = X[test_index,:]
+    y_test = y[test_index]
 
 
 
@@ -33,5 +34,37 @@ error = np.sum(y_est != y_test) / len(y_test)
 print('Coefficients:')
 
 for i in range(len(classNames)):
-    print(classNames[i],': ',logreg.coef_[i,:])
+    print(classNames[i],': ',np.round(logreg.coef_[i,:],2))
+
+
+#%%
+# Select test value
+s = 18
+val = X_test[s:s+1,:]
+x = X_test[s,:]
+
+
+a = logreg.coef_ @ x
+b = y_test[s]
+
+# Apply softmax
+c = np.exp(a)/np.sum(np.exp(a))
+
+for i in range(len(classNames)):
+    print(np.round(c[i],2))
+print('Guess: ',np.argmax(c))  
+print('Predicted: ', logreg.predict(val))
+print('Real: ', b)  
+    
+scores = np.zeros(8) 
+for i in range(len(classNames)):
+    w  = logreg.coef_[i,:]
+    a = w.dot(x)
+    scores[i] = a
+#    c = np.exp(a)/np.sum(np.exp(a))
+#    print(np.max(c))
+#scores -= np.max(scores)
+print('Guess: ',np.argmax(np.exp(scores)/np.sum(np.exp(scores))))
+
+
 
